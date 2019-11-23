a4cb6a4e301430356952ab5e433f02e2086cb759
kidhadez <32069896+kidhadez@users.noreply.github.com>
2019-11-23 13:48:04 -0500

    fuck you
    6 levels + win
    start is maingame

--- UnityGameJamLab4/Assets/Goal.cs
+++ UnityGameJamLab4/Assets/Goal.cs
@@ -9,7 +9,7 @@ public class Goal : MonoBehaviour
     private string gmTag;
     [SerializeField]
     [Header("Fire")]
-    private bool fire;
+    public bool fire;
 
     // Start is called before the first frame update
     void Start()
---
+++ UnityGameJamLab4/Assets/sceneChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneChange : MonoBehaviour
+{
+    public string levelName;
+    public GameObject goal1;
+    public GameObject goal2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (goal1.GetComponent<Goal>().fire && goal2.GetComponent<Goal>().fire)
+        {
+            SceneManager.LoadScene(levelName, LoadSceneMode.Single);
+        }
+    }
+}
