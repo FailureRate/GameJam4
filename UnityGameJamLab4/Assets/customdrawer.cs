0fc77576b6412ed9842e5f9b761ac0878c7690c7
kidhadez <32069896+kidhadez@users.noreply.github.com>
2019-12-15 20:38:50 -0500

    poopy brown healthbar
    addedproperty drawer, chanegs colours between 0 and 100 from red to green. good for health checks, timers etc

---
+++ UnityGameJamLab4/Assets/customdrawer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(float))]
+public class customdrawer : PropertyDrawer
+{
+    // Start is called before the first frame update
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        //base.OnGUI(position, property, label);
+        EditorGUI.BeginProperty(position, label, property);
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+        GUI.backgroundColor = Color.Lerp(Color.red, Color.green, property.floatValue / 100);
+        EditorGUI.PropertyField(position, property, GUIContent.none);
+        EditorGUI.EndProperty();
+    }
+}
